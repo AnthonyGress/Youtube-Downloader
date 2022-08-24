8a8189cf16b7c2be5537c98dc2b755ba5e870287
Anthony <the.techx.guy@gmail.com>
2022-08-24 19:06:39 -0400

    rename repo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 OS="$(uname)"
 UNAME_MACHINE="$(/usr/bin/uname -m)"
 USER_PLATFORM="$OS $UNAME_MACHINE"
-LATEST_VERSION=$(curl -s -L https://api.github.com/repos/anthonygress/youtube-dl/tags | grep '"name":' | head -1 | cut -d: -f2 | cut -c4-8)
+LATEST_VERSION=$(curl -s -L https://api.github.com/repos/anthonygress/mac-the-ripper/tags | grep '"name":' | head -1 | cut -d: -f2 | cut -c4-8)
 
 install_app() {
     echo -e "\n---------------------- Installing Application ----------------------"
@@ -10,16 +10,16 @@ install_app() {
 
     if [[ "$USER_PLATFORM" == "Darwin arm64" ]]
     then
-        curl -sL https://github.com/AnthonyGress/youtube-dl/releases/download/v${LATEST_VERSION}/mac-the-ripper-${LATEST_VERSION}-arm64-mac.zip --output ~/Downloads/mac-the-ripper.zip && unzip -qo ~/Downloads/mac-the-ripper.zip -d /Applications
+        curl -sL https://github.com/AnthonyGress/mac-the-ripper/releases/download/v${LATEST_VERSION}/mac-the-ripper-${LATEST_VERSION}-arm64-mac.zip --output ~/Downloads/mac-the-ripper.zip && unzip -qo ~/Downloads/mac-the-ripper.zip -d /Applications
     elif [[ "$USER_PLATFORM" == "Darwin x86_64" ]]
     then
-        curl -sL https://github.com/AnthonyGress/youtube-dl/releases/download/v${LATEST_VERSION}/mac-the-ripper-${LATEST_VERSION}-mac.zip --output ~/Downloads/mac-the-ripper.zip && unzip -qo ~/Downloads/mac-the-ripper.zip -d /Applications
+        curl -sL https://github.com/AnthonyGress/mac-the-ripper/releases/download/v${LATEST_VERSION}/mac-the-ripper-${LATEST_VERSION}-mac.zip --output ~/Downloads/mac-the-ripper.zip && unzip -qo ~/Downloads/mac-the-ripper.zip -d /Applications
     elif [[ "$USER_PLATFORM" == "Linux arm64" ]]
     then
-         curl -sL https://github.com/AnthonyGress/youtube-dl/releases/download/v${LATEST_VERSION}/mac-the-ripper-${LATEST_VERSION}-arm64.AppImage --output ~/Desktop/mac-the-ripper-arm64.AppImage && chmod +x ~/Desktop/youtube-dl-arm64.AppImage
+         curl -sL https://github.com/AnthonyGress/mac-the-ripper/releases/download/v${LATEST_VERSION}/mac-the-ripper-${LATEST_VERSION}-arm64.AppImage --output ~/Desktop/mac-the-ripper-arm64.AppImage && chmod +x ~/Desktop/mac-the-ripper-arm64.AppImage
     elif [[ "$USER_PLATFORM" == "Linux x86_64" ]]
     then
-         curl -sL https://github.com/AnthonyGress/youtube-dl/releases/download/v${LATEST_VERSION}/mac-the-ripper-${LATEST_VERSION}.AppImage --output ~/Desktop/mac-the-ripper.AppImage && chmod +x ~/Desktop/youtube-dl.AppImage
+         curl -sL https://github.com/AnthonyGress/mac-the-ripper/releases/download/v${LATEST_VERSION}/mac-the-ripper-${LATEST_VERSION}.AppImage --output ~/Desktop/mac-the-ripper.AppImage && chmod +x ~/Desktop/mac-the-ripper.AppImage
     else
         echo "OS not supported - please check the readme for install and support instructions"
         exit 1
@@ -58,7 +58,7 @@ echo
 }
 
 #runtime
-curl https://raw.githubusercontent.com/AnthonyGress/youtube-dl/main/assets/art.txt
+curl https://raw.githubusercontent.com/AnthonyGress/mac-the-ripper/main/assets/art.txt
 install_app
 sleep 2
 install_yt-dl
